40a5a891a7fffe840d0c8f632a129ba67452c5ae
piekstra <piekstra@users.noreply.github.com>
2021-03-25 18:33:36 -0700

    Switching to TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/api/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -26,7 +18,6 @@ function App() {
         >
           Learn React
         </a>
-        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
