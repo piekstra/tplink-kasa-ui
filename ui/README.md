d45e128ffd155596d5af4804d0a8217182c80734
amoreena <amo.hagedorn@gmail.com>
2021-08-22 10:37:42 -0400

    Enforce Linting Styles on the FrontEnd (#17)
    * chore(Linting): for frontend
    
    Co-authored-by: ahagedorn <amo.hagedorn@gmail.com>

--- ui/README.md
+++ ui/README.md
@@ -25,3 +25,10 @@ It correctly bundles React in production mode and optimizes the build for the be
 
 The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
+
+## Linting
+
+_For automatic linting_
+### `npm run lint:fix`
+
+Alternatively, commit from the UI directory
--- ui/src/services/useInterval.tsx
+++ ui/src/services/useInterval.tsx
@@ -1,23 +1,24 @@
 import { useEffect, useRef } from 'react';
 
-export default function useInterval(callback: () => void, delay: number) {
-    const savedCallback = useRef<() => void>();
+export default function useInterval(callback: () => void, delay: number): void {
+  const savedCallback = useRef<() => void>();
 
-    // Remember the latest callback.
-    useEffect(() => {
-        savedCallback.current = callback;
-    }, [callback]);
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-    // Set up the interval.
-    useEffect(() => {
-        function tick() {
-            if (savedCallback.current) {
-                savedCallback.current();
-            }
-        }
-        if (delay !== null) {
-            let id = setInterval(tick, delay);
-            return () => clearInterval(id);
-        }
-    }, [delay]);
+  // Set up the interval.
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
